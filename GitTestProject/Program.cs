ff2ed444529b6b6d901520f42d1e01a209dbb40e
Henrikrom <75252125+Henrikrom@users.noreply.github.com>
2020-12-01 13:46:37 +0100

    Update Program.cs

--- GitTestProject/Program.cs
+++ GitTestProject/Program.cs
@@ -9,15 +9,7 @@ namespace GitTestProject
             Console.WriteLine("Hello World!");
         }
 
-        public void newFeat()
-        {
-            Console.WriteLine("New feature");
-        }
         
-        public void newFeat2()
-        {
-            Console.WriteLine("New feature2");
-        }
     }
 
     
